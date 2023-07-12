7a9fcd3a896198c112f9fd9a1ea253b7c1bf6c19
unknown <salimliaynur@gmail.com>
2023-07-12 11:12:23 +0400

    sum of even and odd

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ for num in range(1, 11):
         sum +=num
 
 print(sum)
+
+
+
+#Sum of even numbers between 1-11
+sum = 0
+for num in range(1, 11):
+    if(num % 2 == 0):
+        sum+= num
+
+print(sum)
